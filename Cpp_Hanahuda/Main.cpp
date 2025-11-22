e0fce44f60b25b38d65ae50e4092d2f6427c9f4b
3BLABLAB <rk3b.0215@gmail.com>
2025-11-23 00:11:54 +0900

    SetUp

---
+++ Cpp_Hanahuda/Main.cpp
@@ -0,0 +1,98 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.16
+
+using BahudaLines = std::array<Array<Huda>, 12>;
+using TehudaLines = std::array<Huda, 8>;
+
+bool BahudaAppeared[12][4];
+
+struct Huda {
+	int width;
+	int height;
+	int month;
+	int order;
+
+public: Huda() :width(40), height(60) {}
+public: Huda(int m, int o) :month(m), order(o), width(40), height(60) {}
+};
+
+void DrawRadialGradientBackground(const ColorF& centerColor, const ColorF& outerColor)
+{
+	Circle{ Scene::Center(), (Scene::Size().length() * 0.5) }
+	.draw(centerColor, outerColor);
+}
+
+void DrawTable(int turn = 0, BahudaLines Bahuda, TehudaLines ATehuda, TehudaLines BTehuda) {
+	//Vec2 CenterPos = Window::Center();
+	Vec2 CenterPos = Scene::Center();
+	Huda huda;
+	int gap = 10;
+	//Render yamahuda
+	RectF(Arg::center(CenterPos), huda.width, huda.height)
+		.drawFrame(1, Palette::Black)
+		.draw(ColorF(0.26, 0.43, 0.43));
+	//Render bahuda
+	//TODO
+	return;
+}
+
+Huda GetNewHuda() {
+	int NewHudaNum = Random(0, 47);
+	int m, o;
+	do
+	{
+		NewHudaNum = Random(0, 47);//0-47
+		m = NewHudaNum / 4;
+		o = NewHudaNum % 12;
+	} while (BahudaAppeared[m][o]);
+	return Huda(m, o);
+}
+
+void SetUp(BahudaLines& Bahuda, TehudaLines& ATehuda, TehudaLines& BTehuda) {
+	//setting bahuda
+	int nonEmptyMonthCount = 0;
+	while (nonEmptyMonthCount < 8)
+	{
+		Huda NewHuda = GetNewHuda();
+		Bahuda[NewHuda.month].push_back(NewHuda.order);
+		for (int i = 0; i < 12; i++)
+		{
+			if (Bahuda[i].isEmpty())
+			{
+				nonEmptyMonthCount++;
+			}
+		}
+	}
+	//setting tehuda
+	for (int i = 0; i < 8; i++) {
+		Huda NewHuda = GetNewHuda();
+		ATehuda[i] = NewHuda;
+		Huda NewHuda = GetNewHuda();
+		BTehuda[i] = NewHuda;
+	}
+}
+
+void Main()
+{
+	// 背景の色を設定する | Set the background color
+	//Scene::SetBackground(ColorF{ 0.3, 0.9, 0.6 });
+	int turn = 0;
+	BahudaLines Bahuda;
+	TehudaLines ATehuda, BTehuda;
+	SetUp(Bahuda, ATehuda, BTehuda);
+
+	while (System::Update())
+	{
+		DrawRadialGradientBackground(ColorF{ 0.2, 0.8, 0.4 }, ColorF{ 0.26, 0.43, 0.35 });
+		DrawTable(turn, Bahuda, ATehuda, BTehuda);
+	}
+}
+
+//
+// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
+//
+// - Release ビルド: 最大限の最適化でビルドします。
+//
+// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
+//
+// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要な場合があります。
+//
